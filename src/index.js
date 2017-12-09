8785f3773280eeca8408dcbb2f580744021ea076
Ian Agpawa <agpawaji@gmail.com>
2017-12-09 11:42:54 -0500

    feat: Add youtube search, change functional App component to class component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import API from './secrets.json';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = API['youtube']
-// Component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos)  => {
+            this.setState({ videos });
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+
+
 }
+
 
 // Render component to DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
